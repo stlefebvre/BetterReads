e3b6f9eb260a7a35253cefd11172c18b7687b684
stlefebvre <lefebvre.shelby@gmail.com>
2018-12-23 13:35:25 -0500

    uploading pexels image, reworking front end HTML & CSS

--- public/js/view.js
+++ public/js/view.js
@@ -1,1 +1,28 @@
+/*
+
+Goodreads API docs: https://www.goodreads.com/api/index#reviews.list
+
+xml-js npm converter: https://www.npmjs.com/package/xml-js
+
+*/
+
 require("dotenv").config();
+
+$(document).ready(function () {
+    $("#search-btn").on("click", function (event) {
+        var url = "https://www.goodreads.com/review/list?v=2";
+        var searchInput = $("#searchInput").val();
+        var list = $("#list").val();
+        var author = $("#author").val().trim();
+        var genre = $("#genre").val().trim();
+        var numPages = $("#numPages").val();
+        url += '?' + $.param({
+            'v': "2",
+            'id': "27136019",
+            'sort': "title",
+            'search[query]': searchInput,
+            'per_page': 50,
+            'num_pages': numPages,
+        });
+    });
+});
